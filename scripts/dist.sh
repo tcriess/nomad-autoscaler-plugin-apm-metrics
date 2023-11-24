4b215e565e61a998aed04995ccb69505a755f633
Thorsten Riess <tr@scientific-wave.com>
2023-11-24 15:19:51 +0100

    fix name in dist script

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -6,5 +6,5 @@ if [ $1 == "windows" ]; then
 fi
 
 CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -ldflags "-s -w" -a -installsuffix cgo -o "dist/metrics${suffix}"
-zip -j dist/cron_$1_$2.zip "dist/metrics${suffix}"
+zip -j dist/metrics_$1_$2.zip "dist/metrics${suffix}"
 rm -rf "dist/metrics${suffix}"
